6e6f0832e644a8b91b54567a872d7f48b3e897c2
Markuss Mišķis <markuss.miskis@gmail.com>
2024-08-20 20:07:11 +0300

    Fix curly brace in xserver.nix

--- nixos/modules/xserver.nix
+++ nixos/modules/xserver.nix
@@ -1,11 +1,10 @@
 {pkgs, ...}: {
   services.xserver = {
-      enable = true;
-      windowManager.i3.enable = true;
-      displayManager.lightdm.enable = true;
-      excludePackages = with pkgs; [
-        xterm
-      ];
-    };
+    enable = true;
+    windowManager.i3.enable = true;
+    displayManager.lightdm.enable = true;
+    excludePackages = with pkgs; [
+      xterm
+    ];
   };
 }
